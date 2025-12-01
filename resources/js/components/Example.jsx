997994b7e25aa85a573ffda560857496879f785e
Alexis Morales <alexis.morales@zascita.com>
2025-12-01 14:36:52 -0600

    Instalación de react y tailwindcss

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header text-red-600">Example Component of Alexis</div>
+                         <h1 class="text-3xl font-bold underline">
+                            Hello world!
+                        </h1>
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    const Index = ReactDOM.createRoot(document.getElementById("example"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
